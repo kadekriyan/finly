df4004cd946c55469bd27861dc98482713ac4194
kadekriyan <kadekriyan16@gmail.com>
2025-06-28 16:09:53 +0700

    update : color theme

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,9 +1,10 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-class AppColor {
-  static const Color primary = Color(0xFFECEDEE);
-  static Color blue = Colors.blue.shade600;
+class AppColors {
+  static const Color primaryColor = Color(0xFFECEDEE);
+  static const Color blue = Color(0xFF3B82F6);
+  static const Color black = Color(0xFF000000);
   static const Color white = Colors.white;
-  static const Color black = Colors.black;
-  static const Color grey = Colors.grey;
+  static const Color blueBackground = Color(0xFF62B3FF);
+  static const Color grey = Color.fromARGB(255, 88, 88, 88);
 }
